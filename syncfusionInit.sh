468c22123d7ab6ae85018b0a02d0c05dac293102
Mathieu <math.per@criptext.com>
2020-05-11 19:45:50 -0400

    add syncfusion POC and script to adjust packages

---
+++ syncfusionInit.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+npm install @syncfusion/ej2-angular-spreadsheet --save
+npm i @angular/common@9.1.3
+npm i @angular/compiler@9.1.3
+npm i @angular/core@9.1.3
+npm i @angular/forms@9.1.3
+npm i @angular/platform-browser@9.1.3
+npm i @angular/platform-browser-dynamic@9.1.3
+npm i @angular/animations@9.1.3
+npm i @angular/router@9.1.3
