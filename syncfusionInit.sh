029f60097d11d8c1fe80bb916951497d4ff2cad3
Mathieu <math.per@criptext.com>
2020-05-12 10:39:48 -0400

    css correction + script formatting

--- syncfusionInit.sh
+++ syncfusionInit.sh
@@ -3,7 +3,13 @@
 # before doing this be sure to do habitual Angular CLI project creation
 # commands
 
+# see https://ej2.syncfusion.com/angular/documentation/spreadsheet/getting-started/
+# for more infos
+
+# install syncfusion
 npm install @syncfusion/ej2-angular-spreadsheet --save
+
+# downgrade packages to correct version
 npm i @angular/common@9.1.3
 npm i @angular/compiler@9.1.3
 npm i @angular/core@9.1.3
