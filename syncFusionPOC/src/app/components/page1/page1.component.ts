4f18419cb5fdd645ac59406f4b4509f14bfa552b
mathieu <math.per@hotmail.com>
2020-05-26 17:15:29 -0400

    WIP Json object being passed through components but problem with format

--- syncFusionPOC/src/app/components/page1/page1.component.ts
+++ syncFusionPOC/src/app/components/page1/page1.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { isUndefined } from 'util';
+import { SpreadsheetComponent } from '@syncfusion/ej2-angular-spreadsheet';
+import * as $ from 'jquery'; window["$"] = $; window["jQuery"] = $;
+
 
 @Component({
   selector: 'app-page1',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page1.component.scss']
 })
 export class Page1Component implements OnInit {
+  spreadsheetSaved: any;
+  // to show or not the div containing the readonly spreadsheet
+  isInvisible: boolean;
+
+  @ViewChild('edxSpreadsheet') public spreadsheetObj: SpreadsheetComponent;
 
   constructor() { }
 
   ngOnInit(): void {
+    
   }
+  
+  public ngAfterViewInit(): void {
+    // assign the value of the data passed from the router (if so) from page2Component
+    this.spreadsheetSaved = window.history.state.data;
+    console.log(this.spreadsheetSaved); // test
+    console.log('yess'); // test
+    if(!(isUndefined(this.spreadsheetSaved))){
+      this.spreadsheetObj.openFromJson(JSON.stringify(this.spreadsheetSaved.jsonObject));
+      this.isInvisible = false;
+    } else {
+      this.isInvisible = true;
+    }
+  }
+  
+  loadFromJSON() {
+    var excelObj = $("#spreadsheet").data("ejSpreadsheet");
+    excelObj.loadFromJSON(this.spreadsheetSaved);
+    console.log("loaded");
+}
 
 }
--- syncFusionPOC/src/app/components/page2/page2.component.ts
+++ syncFusionPOC/src/app/components/page2/page2.component.ts
@@ -12,64 +12,83 @@ import { Router } from '@angular/router';
 export class Page2Component implements OnInit, OnDestroy, AfterViewInit {
   //xlObj:Object;
   //( private readonly _spreadSheetService: SpreadSheetService
-  constructor(private updaterService: UpdaterService,
-    private readonly _router: Router) { }
+  constructor(
+    private updaterService: UpdaterService,
+    private readonly _router: Router,
 
-  @ViewChild('ejs-spreadsheet') public spreadsheetObj: SpreadsheetComponent;
-  public query: Query = new Query().select(['OrderID', 'CustomerID', 'ShipName', 'ShipCity', 'ShipCountry', 'Freight']).take(200);
-  public data: DataManager = new DataManager({
-    url: 'https://js.syncfusion.com/demos/ejServices//wcf/Northwind.svc/Orders',
-    crossDomain: true
-  });
-
-  public ngOnInit(): void {
-    this.data = this.data;
-  }
-
-  // Use ref DOM. Encapsulation around the nativeElement
-  public ngAfterViewInit(): void {
-    // Can access View Child
-  }
-
-  public ngOnDestroy(): void {
-    // this._subscriptions.unsubscribe();
-  }
-
-  // public vu que referee dans un template
-  public saveSpreadsheet($event: Event): void {
-    // cest ici quon va appeler le service yeah yeah
-    // this._subscriptions.add
-    // TODO: private readonly _subcriptions = new Subscription();
-
-    // Can access View Child
-    const json = this.saveAsJson();
-    this.updaterService.saveSpreadsheetApi(json, null)
+    ) { }
+    
+    @ViewChild('edxSpreadsheet') public spreadsheetObj: SpreadsheetComponent;
+    
+    public query: Query = new Query().select(['OrderID', 'CustomerID', 'ShipName', 'ShipCity', 'ShipCountry', 'Freight']).take(200);
+    
+    public data: DataManager = new DataManager({
+      url: 'https://js.syncfusion.com/demos/ejServices//wcf/Northwind.svc/Orders',
+      crossDomain: true
+    });
+    
+    
+    public ngOnInit(): void {
+      //this.data = this.data;
+    }
+    
+    // Use ref DOM. Encapsulation around the nativeElement
+    public ngAfterViewInit(): void {
+      /*
+      // Can access View Child
+      let xlObj = $("#spreadsheet").data("ejSpreadsheet");
+      // To save the worksheet on Excel format.
+      xlObj.XLExport.export("Excel", "sample");
+      */
+    }
+    
+    public ngOnDestroy(): void {
+      // this._subscriptions.unsubscribe();
+    }
+    
+    // public vu que referee dans un template
+    public saveSpreadsheet($event: Event): void {
+      // cest ici quon va appeler le service yeah yeah
+      // this._subscriptions.add
+      // TODO: private readonly _subcriptions = new Subscription();
+      
+      // Can access View Child
+      const json = this.saveAsJson();
+      
+      /*
+      this.updaterService.saveSpreadsheetApi(json, null)
       .subscribe(
         result => {
           const result2 = result;
           // le result success
           this._router.navigateByUrl('/page1');
-          // this._router.navigate()
         },
-        error => {
+        error => {}
+        );
+        */
+      }
+      /*
+      saveAsFile() {
+        var xlObj = $("#spreadsheet").data("ejSpreadsheet");
+        xlObj.XLExport["export"](ej.Spreadsheet.exportType.Excel);
+      }
+      */
+      
+      //
+      private saveAsJson(): any {
+        this.spreadsheetObj.saveAsJson()
+        .then((obj) => {
+          console.log(obj);
+          // objet retourne JSON
+          this.goToComponentA(obj);
 
-        }
-      );
-  }
-
-  saveAsFile() {
-    var xlObj = $("#spreadsheet").data("ejSpreadsheet");
-    xlObj.XLExport["export"](ej.Spreadsheet.exportType.Excel);
-  }
-
-
-  private saveAsJson(): any {
-    this.spreadsheetObj.saveAsJson()
-      .then(() => {
-        // objet retourne JSON
-      })
-      .finally(() => {
-        alert('Promise ready');
-      })
-  }
-}
+        })
+        .finally(() => {
+          alert('Promise ready');
+        });
+      }
+      goToComponentA(passedObj: Object): void {
+        this._router.navigate(['/page1'], {state: {data: passedObj}});
+      }
+      
+    }
